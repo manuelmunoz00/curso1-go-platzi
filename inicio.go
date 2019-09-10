a348a10dd68004e0de47f906c031245bfb8f12cd
Manuel Muñoz <manuel.munoz00@gmail.com>
2019-09-09 21:56:58 -0300

    se agrega struct

--- inicio.go
+++ inicio.go
@@ -2,11 +2,31 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
-func main()  {
-	//Comentario
-	time.Sleep(time.Second * 5)
+type Cliente struct {
+	rut string
+	nombres string
+	apellidos string
+	edad int
+}
+
+func main() {
+	var persona1 = Cliente {
+		rut: "16006363-7",
+		nombres: "Manuel Alejandro",
+		apellidos:"Muñoz Ayala",
+		edad:34}
+	var suma int
+	suma = 8 + 9
+	fmt.Println(suma)
+	//time.Sleep(time.Second * 5)
 	fmt.Println("Hello world")
+	var nombre string = "Manuel"
+	fmt.Println(nombre)
+	pais := "Chile"
+	fmt.Println(pais)
+	fmt.Println(persona1)
+
+	
 }
